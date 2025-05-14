b8b45debbf52fd1b7b7ac4f96045c508150289a8
Jimmy <sc171@illinois.edu>
2025-05-14 11:29:52 -0500

    correct

--- test.py
+++ test.py
@@ -13,12 +13,12 @@ import numpy as np
 import joblib
 import os
 
-# 安全存储 API Key
-API_KEY = "a9e680c901b64650acd211526250304"
+# API Key
+API_KEY = "Key"
 LOCATION = "Illinois"
 base_url = "http://api.weatherapi.com/v1/history.json"
 
-# 当前时间
+
 now = datetime.now()
 dates_needed = list(set([
     now.strftime("%Y-%m-%d"),
@@ -27,7 +27,6 @@ dates_needed = list(set([
 
 weather_data = []
 
-# 获取天气数据
 for date_str in dates_needed:
     url = f"{base_url}?key={API_KEY}&q={LOCATION}&dt={date_str}"
     try:
@@ -35,7 +34,7 @@ for date_str in dates_needed:
         response.raise_for_status()
         data = response.json()
     except requests.RequestException as e:
-        print(f"请求失败: {e}")
+        print(f"error: {e}")
         continue
 
     if "forecast" in data:
@@ -56,34 +55,32 @@ for date_str in dates_needed:
                         "Wind_Bearing_deg": hour_data["wind_degree"]
                     })
         except KeyError:
-            print(f"数据结构异常: {data}")
+            print(f"error: {data}")
             continue
 
 if not weather_data:
-    print("未获取到天气数据")
+    print("cannot find weather")
     exit()
 
-# 转换为 NumPy 数组
+
 df = pd.DataFrame(weather_data).to_numpy()
 
-# 归一化
+
 scaler = MinMaxScaler()
 df_scaled = scaler.fit_transform(df)
 
 # Reshape the data to match the LSTM model input shape
 df_reshaped = df_scaled.reshape((df_scaled.shape[0], 1, df_scaled.shape[1]))  # Shape (10, 1, 7)
 
-# 加载模型
 try:
     mc = joblib.load("weather_lstm.pkl")
 except FileNotFoundError:
-    print("模型文件未找到")
+    print("model not found")
     exit()
 
-# 预测
 dapre = mc.predict(df_reshaped)
 
-# 反归一化温度
+
 def inverse_scale_temp(scaled_temp, scaler, feature_index=0):
     dummy = np.zeros((scaled_temp.shape[0], df.shape[1]))
     dummy[:, feature_index] = scaled_temp.flatten()
